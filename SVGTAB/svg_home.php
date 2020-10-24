e584558c4104d61428cda1e5d4e742adffd5c3ae
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-10-25 00:14:53 +0800

    Update svg_home.php

--- SVGTAB/svg_home.php
+++ SVGTAB/svg_home.php
@@ -8,5 +8,6 @@ $module = (isset($_GET['mod']) && $_GET['mod'] != '') ? $_GET['mod'] : '';
   <link href="css/style.css" rel="stylesheet">
 <div id="content">
 <a href="SVGTAB/graphics" class="button2">5.4 Engage - Learning Task 5.1</a> <br>
-<a href="SVGTAB/textSVG" class="button2">6.4 Engage - Learning Task 6.1</a>
+<a href="SVGTAB/textSVG" class="button2">6.4 Engage - Learning Task 6.1</a><br>
+<a href="SVGTAB/svg_animation" class="button2">6.4 Engage - Learning Task 6.2</a>
 </div>
