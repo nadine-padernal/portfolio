1f1a5b3e29b35af49f8eff43b3eac4b4d414ab0c
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-11-21 22:43:25 +0800

    Update svg_home.php

--- SVGTAB/svg_home.php
+++ SVGTAB/svg_home.php
@@ -10,4 +10,5 @@ $module = (isset($_GET['mod']) && $_GET['mod'] != '') ? $_GET['mod'] : '';
 <a href="SVGTAB/graphics" class="button2">5.4 Engage - Learning Task 5.1</a> <br>
 <a href="SVGTAB/textSVG" class="button2">6.4 Engage - Learning Task 6.1</a><br>
 <a href="SVGTAB/svg_animation" class="button2">6.4 Engage - Learning Task 6.2</a>
+<a href="SVGTAB/floom" class="button2">CLE1: Culminating Learning Evidence (Midterm)</a>
 </div>
